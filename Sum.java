8ccc91eb0544db8248f5afc4b5b4a9431c230b46
tsaisreekar <thimmavajjalasaisreekar@gmail.com>
2025-08-09 14:05:23 +0530

    Added Hello.java with hello this is sreekar

--- Sum.java
+++ Sum.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number: ");
+        System.out.print("Enter first number here: ");
         int a = sc.nextInt();
-        System.out.print("Enter second number: ");
+        System.out.print("Enter second number here: ");
         int b = sc.nextInt();
         int sum = a + b;
         System.out.println("Sum = " + sum);
