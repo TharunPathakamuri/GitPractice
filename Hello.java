a07e705c7a54d8dce9d7b651907455ca834ea10d
udaykiran1218 <udaykiran1218@gmail.com>
2025-08-09 13:30:41 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, uday");
+        System.out.println("Hello, my name is uday");
     }
 }
